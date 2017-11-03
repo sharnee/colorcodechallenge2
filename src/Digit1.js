76952dfbdf215784fcd6be0b5656b192ae8a41a9
Shar'nee Francis <shareefrancis11@gmail.com>
2017-11-02 23:40:48 -0500

    Completed the functionality of the Calculator. Added the silver and gold data sets. Updated the order of operations for the h1 sum display.

--- src/Digit1.js
+++ src/Digit1.js
@@ -9,10 +9,11 @@ class Digit1 extends Component {
     let bands = colorcode;
     this.state = {
       colors: bands,
-      options: [ 0 , 0, 0],
+      options: [ 0 , 0, 0, 0],
       option0: 0,
       option1: 0,
       option2: 0,
+      option3: 0,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -38,24 +39,33 @@ class Digit1 extends Component {
 
   render() {
   	console.log(this.state, 'the state')
+  	const { option0, option1, option2, option3} = this.state
+  	let optionValue = function (optionsIndex, code){
+  	 	if (optionsIndex == 2) {return code.multiplier}
+  	 	if (optionsIndex == 3) { return code.tolerance}
+  	 	return code.digit
+  	 }
     return (
       <div>
 	      <form onSubmit={this.handleSubmit}>
 	        {console.log('onSubmit is working')}
 	        {this.state.options.map((option, optionsIndex) => {
-	        	return <div>
-			        <label>Pick your Color:</label>
-			        <select name={`option${optionsIndex}`} onChange={(e) => this.handleChange(e)}>
-			          { colorcode.map((code, index) => (
-    	                 <option key={index} value={optionsIndex == 2 ?code.multiplier : code.digit}>{code.color}</option>
-                       ))}
-			        </select>
-		       </div>
+	        	return (
+	        		<div>
+				        <label>Pick your Color:</label>
+				        <select name={`option${optionsIndex}`} onChange={(e) => this.handleChange(e)}>
+				          { colorcode.map((code, index) => (
+	    	                 <option key={index} value={optionValue(optionsIndex, code)}>{code.color}</option>
+	                       ))}
+				        </select>
+		         	</div>
+	          	)
 	        })}
 	   
 	       <input type="submit" value="Submit"/>
 	      </form>
-	      <h1>{this.state.digit}</h1>
+	      <h1>{(option0 + option1) * option2 * (1 - option3)}</h1>
+	      <h1>{(option0 + option1) * option2 * (1 + option3)}</h1>
       </div>
     )
   }
--- src/colorcode.js
+++ src/colorcode.js
@@ -9,55 +9,67 @@ const colorcode = [
   color : "Brown",
   digit : "1",
   multiplier :"10",
-  tolerance: " "
+  tolerance: ".01"
 }, 
 {
   color : "Red",
   digit : 2,
   multiplier : 100,
-  tolerance: " "
+  tolerance: ".02"
 }, 
 {
   color : "Orange",
   digit : 3,
   multiplier : 1000,
-  tolerance: " "
+  tolerance: ".05"
 }, 
 {
   color : "Yellow",
   digit : 4,
   multiplier : 10000,
-  tolerance: " "
+  tolerance: ".05"
 }, 
 {
   color : "Green",
   digit : 5,
   multiplier : 100000,
-  tolerance: " "
+  tolerance: ".005"
 }, 
 {
   color : "Blue",
   digit : 6,
   multiplier : 1000000,
-  tolerance: " "
+  tolerance: ".025"
 }, 
 {
   color : "Violet",
   digit : 7,
   multiplier : 10000000,
-  tolerance: " "
+  tolerance: ".001"
 }, 
 {
   color : "Gray",
   digit : 8,
   multiplier : 100000000,
-  tolerance: " "
+  tolerance: ".0005"
 }, 
 {
   color : "White",
   digit : 9,
   multiplier : 1000000000,
-  tolerance: " "
+  tolerance: ""
+}, 
+{
+  color : "silver",
+  digit : 0,
+  multiplier : 0,
+  tolerance: ".1"
+}, 
+{
+  color : "gold",
+  digit : 0,
+  multiplier : 0,
+  tolerance: ".05"
 }, 
 ];
 
